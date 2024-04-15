50475c5e1aa1e2d0c2cac881cf81bd2da2cf64da
rodrigoconze <rodriconze@gmail.com>
2024-04-14 22:06:10 -0300

    feat(Department): [RED] attribute door number

---
+++ TestDomain/TestDepartment.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Domain;
+
+namespace TestDomain
+{
+    [TestClass]
+    public  class TestDepartment
+    {
+        private Department department;
+
+        [TestMethod]
+        public void TestDoorNumber()
+        {
+            department = new Department();
+            department.DoorNumber = 1;
+            Assert.AreEqual(1, department.DoorNumber);
+        }
+
+    }
+}
