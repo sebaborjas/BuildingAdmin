ee282ade8e28e8c7c810af033989365bd1926b61
Sebastian Borjas <sebastianborjas2000@gmail.com>
2024-04-15 13:00:20 -0300

    add(TestDomain): TestMainenanceOperator class & use cases

---
+++ TestDomain/TestMaintenanceOperator.cs
@@ -0,0 +1,85 @@
+namespace TestDomain;
+
+[TestClass]
+public class TestMaintenanceOperator
+{
+  [TestInitialize]
+  public void TestInitialize()
+  {
+  }
+
+  [TestMethod]
+  public void EmptyNameException()
+  {
+  }
+
+  [TestMethod]
+  public void CorrectName()
+  {
+  }
+  
+  [TestMethod]
+  public void EmptyLastNameException()
+  {
+  }
+  
+  [TestMethod]
+  public void CorrectLastName()
+  {
+  }
+  
+  [TestMethod]
+  public void EmptyEmailException()
+  {
+  }
+
+  [TestMethod]
+  public void WrongEmailFormalException()
+  {
+  }
+
+  [TestMethod]
+  public void CorrectEmail()
+  {
+  }
+  
+  [TestMethod]
+  public void EmptyPasswordException()
+  {
+  }
+  
+  [TestMethod]
+  public void WrongPasswordLength()
+  {
+  }
+  
+  [TestMethod]
+  public void PasswordNotFollowPolicy()
+  {
+  }
+  
+  [TestMethod]
+  public void CorrectPassword()
+  {
+  }
+  
+  [TestMethod]
+  public void GetEmptyListOfTickets()
+  {
+  }
+  
+  [TestMethod]
+  public void GetTicketsCorrect()
+  {
+  }
+  
+  [TestMethod]
+  public void CloseTicket()
+  {
+  }
+  
+  [TestMethod]
+  public void TakeTicket()
+  {
+  }
+}
