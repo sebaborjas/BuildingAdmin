14e89fdb399f3c10464dafba4563d71742a2de40
Agustina Martinez <agustinamartinez1044@gmail.com>
2024-04-17 15:40:34 -0300

    feat(Building): [REFACTOR] attribute name

--- Domain/Building.cs
+++ Domain/Building.cs
@@ -4,33 +4,41 @@ namespace Domain;
 
 public class Building
 {
-    private int id;
-    private string name;
+    private int _id;
+    private string _name;
 
     public int Id
     {
-        get => id;
+        get => _id;
         set
         {
             if (value < 0)
             {
                 throw new ArgumentOutOfRangeException();
             }
-            id = value;
+            _id = value;
         }
     }
 
    public string Name
     {
-        get => name;
+        get => _name;
         set
         {
-            if (string.IsNullOrEmpty(value))
+            if (string.IsNullOrWhiteSpace(value))
             {
-                throw new ArgumentNullException();
+                throw new EmptyFieldException();
+            }else if (!IsNameValid(value))
+            {
+                throw new InvalidDataException("El nombre solo puede contener letras y espacios");
             }
-            name = value;
+            _name = value;
         }
     }   
+
+    private bool IsNameValid(string name)
+    {
+        return name.All(c => char.IsLetter(c) || char.IsWhiteSpace(c));
+    }
 
 }
