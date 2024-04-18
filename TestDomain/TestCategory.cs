54e4397ae3823a43cf6c74e070fb822850336e2e
Agustina Martinez <agustinamartinez1044@gmail.com>
2024-04-18 13:22:43 -0300

    feat(Category): attribute name

---
+++ TestDomain/TestCategory.cs
@@ -0,0 +1,23 @@
+﻿using Domain;
+using Exceptions;
+namespace TestDomain;
+
+[TestClass]
+public class TestCategory
+{
+    Category Category;
+
+    [TestInitialize]
+    public void TestInitialize()
+    {
+        Category = new Category();
+    }
+
+    [TestMethod]
+    public void TestName()
+    {
+        Category.Name = "Category";
+        Assert.AreEqual("Category", Category.Name);
+    }
+
+}
