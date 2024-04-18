e478ff9301156caa5120f6522f5d5a0aee7ebad4
Sebastian Borjas <sebastianborjas2000@gmail.com>
2024-04-17 22:44:33 -0300

    feat(Invitation): [GREEN] set empty email attribute exception

--- Domain/Invitation.cs
+++ Domain/Invitation.cs
@@ -1,7 +1,20 @@
+using Exceptions;
 namespace Domain
 {
   public class Invitation
   {
-    public string Email { get {return "hola@hola.com";} set{} }
+    public string Email { 
+      get 
+      {
+        return "hola@hola.com";
+      } 
+      set
+      {
+        if (value == "")
+        {
+          throw new EmptyFieldException();
+        }
+      } 
+    }
   }
 }
