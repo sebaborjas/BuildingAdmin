38f98a67a2f5772cc75a9762c22936e02ce37b31
Sebastian Borjas <sebastianborjas2000@gmail.com>
2024-04-20 20:42:08 -0300

    feat(TestDomain): [GREEN] test administrator id

--- Domain/Administrator.cs
+++ Domain/Administrator.cs
@@ -2,6 +2,15 @@ namespace Domain
 {
   public class Administrator : User
   {
-    
+    private int _id;
+
+    public int Id
+    {
+      get => _id;
+      set 
+      { 
+        _id = value; 
+      }
+    }
   }
 }
--- TestDomain/TestAdministrator.cs
+++ TestDomain/TestAdministrator.cs
@@ -1,18 +1,23 @@
+using Domain;
+
 namespace TestDomain
 {
   [TestClass]
   public class TestAdministrator
   {
+    private Administrator _administrator;
+
     [TestInitialize]
     public void SetUp()
     {
-      
+     _administrator = new Administrator(); 
     }
     
     [TestMethod]
-    public void TestMethod1()
+    public void TestId()
     {
-      
+      _administrator.Id = 1;
+      Assert.AreEqual(1, _administrator.Id);
     }
   }
 }
