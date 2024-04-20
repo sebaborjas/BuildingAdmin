058f856ac8c0d51c7db08a9f6aef94080e7d9a28
Sebastian Borjas <sebastianborjas2000@gmail.com>
2024-04-20 20:34:54 -0300

    feature(Domain): add Administrator class & TestClass

---
+++ TestDomain/TestAdministrator.cs
@@ -0,0 +1,18 @@
+namespace TestDomain
+{
+  [TestClass]
+  public class TestAdministrator
+  {
+    [TestInitialize]
+    public void SetUp()
+    {
+      
+    }
+    
+    [TestMethod]
+    public void TestMethod1()
+    {
+      
+    }
+  }
+}
