6ef640ac0d548fc933b27bd132004e91a7b0a89a
Sebastian Borjas <sebastianborjas2000@gmail.com>
2024-04-21 20:49:26 -0300

    feat(DataAccess): add BuildingAdminContext initial configuration

--- DataAccess/BuildingAdminContext.cs
+++ DataAccess/BuildingAdminContext.cs
@@ -1,16 +1,39 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using Domain;
 
 namespace DataAccess
 {
     public class BuildingAdminContext : DbContext
     {
+        public virtual DbSet<User> Users { get; set; }
+
+        public virtual DbSet<Invitation> Invitations { get; set; }
+
+
         public BuildingAdminContext(DbContextOptions<BuildingAdminContext> options) : base(options)
         {
         }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                string directory = Directory.GetCurrentDirectory();
+
+                IConfigurationRoot configuration = new ConfigurationBuilder()
+                .SetBasePath(directory)
+                .AddJsonFile("appsettings.json")
+                .Build();
+
+                var connectionString = configuration.GetConnectionString(@"BuildingAdmin");
+
+                optionsBuilder.UseSqlServer(connectionString);
+            }
         }
     }
 }
