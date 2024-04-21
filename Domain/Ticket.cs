90a6d44c167eed629bcd9358cf7dabeb67b930ba
Agustina Martinez <agustinamartinez1044@gmail.com>
2024-04-21 12:52:05 -0300

    feat(Ticket): refactor status and dates

--- Domain/Ticket.cs
+++ Domain/Ticket.cs
@@ -8,17 +8,14 @@ public class Ticket
     private int _id;
     private string _description;
     public DateTime CreationDate { get; private set; } = DateTime.Now.Date;
-    private DateTime _attentionDate;
-    private DateTime _closingDate;
-    private TimeSpan _attentionTime;
-    private TimeSpan _closingTime;
     private Apartment _apartment;
     private float _totalCost;
     public User AssignedTo { get; set; }
     private User _createdBy;
     public Category Category { get; set; }
     public Status Status { get; set; } = Status.Open;
-
+    public DateTime AttentionDate { get; private set; }
+    public DateTime ClosingDate { get; private set; }
 
     public int Id
     {
@@ -92,113 +89,63 @@ public class Ticket
         }
     }
 
-    public DateTime AttentionDate
-    {
-        get { return _attentionDate; }
-        private set
-        {
-            if (_attentionDate == default(DateTime) && value >= CreationDate)
-            {
-                _attentionDate = value;
-            }
-            else if (_attentionDate != default(DateTime))
-            {
-                throw new InvalidOperationException("La fecha de atención ya fue establecida");
-            }
-            else
-            {
-                throw new InvalidOperationException("La fecha de atención no puede ser anterior a la fecha de creación");
-            }
-        }
-    }
-
-    public DateTime ClosingDate
-    {
-        get { return _closingDate; }
-        private set
-        {
-            if (_closingDate == default(DateTime) && value >= _attentionDate)
-            {
-                _closingDate = value;
-            }
-            else if (_closingDate != default(DateTime))
-            {
-                throw new InvalidOperationException("La fecha de cierre ya fue establecida");
-            }
-            else
-            {
-                throw new InvalidOperationException("La fecha de cierre no puede ser anterior a la fecha de atención");
-            }
-        }
-    }
-
-    public TimeSpan AttentionTime
-    {
-        get => _attentionTime;
-        set => _attentionTime = value;
-    }
-
-    public TimeSpan ClosingTime
-    {
-        get => _closingTime;
-        set => _closingTime = value;
-    }
-
-    public void SetAttention(DateTime attentionDateTime)
-    {
-        if (_attentionDate == default(DateTime) && attentionDateTime.Date >= CreationDate)
-        {
-            _attentionDate = attentionDateTime.Date;
-            _attentionTime = attentionDateTime.TimeOfDay;
-        }
-        else if (_attentionDate != default(DateTime))
-        {
-            throw new InvalidOperationException("La fecha de atención ya fue establecida anteriormente.");
-        }
-        else
-        {
-            throw new InvalidOperationException("La fecha de atención no puede ser anterior a la fecha de creación.");
-        }
-    }
-
-    public void SetClosing(DateTime closingDateTime)
-    {
-        if (_closingDate == default(DateTime) && closingDateTime >= _attentionDate)
-        {
-            if (_closingDate == _attentionDate && _closingTime <= _attentionTime)
-            {
-                throw new ArgumentException("La hora de cierre debe ser posterior a la hora de atención");
-            }
-
-            _closingDate = closingDateTime.Date;
-            _closingTime = closingDateTime.TimeOfDay;
-        }
-        else if (_closingDate != default(DateTime))
-        {
-            throw new InvalidOperationException("La fecha de cierre ya fue establecida anteriormente.");
-        }
-        else
-        {
-            throw new InvalidOperationException("La fecha de cierre no puede ser anterior a la fecha de atención.");
-        }
-    }
-
-    public void ChangeStatus(Status newStatus)
+    public void ProcessRequest(Status newStatus, DateTime? dateTime)
     {
         if (newStatus == Status.Open)
         {
-            throw new InvalidOperationException("No se puede cambiar el estado a abierto");
+            ProcessOpenStatus();
         }
-        else if (newStatus == Status.Closed && _closingDate == default(DateTime))
+        else if (newStatus == Status.InProgress)
         {
-            throw new InvalidOperationException("No se puede cerrar un ticket sin fecha de cierre");
+            ProcessInProgressStatus(dateTime);
         }
-        else if (newStatus == Status.InProgress && _attentionDate == default(DateTime))
+        else if (newStatus == Status.Closed)
         {
-            throw new InvalidOperationException("No se puede cambiar el estado a En Progreso sin fecha de atención");
+            ProcessClosedStatus(dateTime);
         }
-        Status = newStatus;
-
+        else
+        {
+            throw new ArgumentException("Estado de solicitud no válido");
+        }
     }
 
+    private void ProcessOpenStatus()
+    {
+        if (Status != Status.Open)
+        {
+            throw new InvalidOperationException("No se puede cambiar el estado a abierto");
+        }
+    }
+
+    private void ProcessInProgressStatus(DateTime? dateTime)
+    {
+        if (dateTime == null)
+        {
+            throw new ArgumentNullException(nameof(dateTime), "La fecha de inicio es requerida");
+        }
+
+        AttentionDate = dateTime.Value;
+        Status = Status.InProgress;
+    }
+
+    private void ProcessClosedStatus(DateTime? dateTime)
+    {
+        if (Status != Status.InProgress)
+        {
+            throw new InvalidOperationException("No se puede cambiar el estado a cerrado sin estar en progreso");
+        }
+
+        if (dateTime == null)
+        {
+            throw new ArgumentNullException(nameof(dateTime), "La fecha de cierre es requerida");
+        }
+
+        if (dateTime.Value < AttentionDate)
+        {
+            throw new ArgumentException("La fecha de cierre no puede ser anterior a la fecha de atención");
+        }
+
+        ClosingDate = dateTime.Value;
+        Status = Status.Closed;
+    }
 }
