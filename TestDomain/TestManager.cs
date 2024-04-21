bcb5c698c3e82f68b50a0fb6491d4d2b09250613
Agustina Martinez <agustinamartinez1044@gmail.com>
2024-04-21 19:34:55 -0300

    feat(Manager): [RED] empty name

---
+++ TestDomain/TestManager.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Domain;
+
+namespace TestDomain
+{
+    [TestClass]
+    public class TestManager
+    {
+
+        [TestMethod]
+        public void EmptyName()
+        {
+            Manager m = new Manager();
+            string name = "";
+            m.Name = name;
+        }
+
+    }
+}
