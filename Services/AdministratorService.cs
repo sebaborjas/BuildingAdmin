ab96e28ccaf0504903bbe5e41a468dbfb5753f90
Sebastian Borjas <sebastianborjas2000@gmail.com>
2024-04-22 21:50:45 -0300

    feat(TestAdministratorService): [GREEN] Test create user

--- Services/AdministratorService.cs
+++ Services/AdministratorService.cs
@@ -1,6 +1,20 @@
+using Domain;
+using IDataAcess;
+
 namespace Services;
 
 public class AdministratorService
 {
-  
+  private readonly IGenericRepository<Administrator> _repository;
+
+  public AdministratorService (IGenericRepository<Administrator> repository)
+  {
+    _repository = repository;
+  }
+
+  public void Insert(Administrator administrator)
+  {
+    _repository.Insert(administrator);
+    _repository.Save();
+  }
 }
--- TestServices/TestAdministratorService.cs
+++ TestServices/TestAdministratorService.cs
@@ -1,8 +1,41 @@
+using Domain;
 using Services;
+using Moq;
+using IDataAcess;
 
 namespace TestServices;
 
+[TestClass]
 public class TestAdministratorService
 {
-  
+  [TestInitialize]
+  public void SetUp()
+  {
+    // Initialize the test
+  }
+
+  [TestMethod]
+  public void CreateCorrectAdministrator()
+  {
+    var mock = new Mock<IGenericRepository<Administrator>>(MockBehavior.Strict);
+
+    mock.Setup(r => r.Save()).Verifiable();
+
+    var service = new AdministratorService(mock.Object);
+
+    var administrator = new Administrator
+    {
+      Name = "Fernando",
+      LastName = "Alonso",
+      Email = "elnano@padre.com",
+      Password = "Nano.1234"
+    };
+
+    mock!.Setup(r => r.Insert(administrator));
+    service!.Insert(administrator);
+
+    // Assert
+    mock.VerifyAll();
+  }
+
 }
