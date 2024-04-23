06eda960b03046527d43860e58cb9e85fcdf2923
Agustina Martinez <agustinamartinez1044@gmail.com>
2024-04-22 21:45:44 -0300

    feat(DataAccess): [RED] insert manager

---
+++ DataAccess/ManagerRepository.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Domain;
+
+namespace DataAccess
+{
+    public class ManagerRepository : GenericRepository<Manager>
+    {
+        public ManagerRepository(DbContext context)
+        {
+            Context = context;
+        }
+    }
+}
---
+++ TestDataAccess/TestManagerRepository.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel;
+
+using Microsoft.Data.Sqlite;
+using Microsoft.EntityFrameworkCore;
+
+using Domain;
+using DataAccess;
+
+namespace TestDataAccess
+{
+    [TestClass]
+    public class TestManagerRepository
+    {
+        private BuildingAdminContext _context;
+        private SqliteConnection _connection;
+        private ManagerRepository _repository;
+
+        [TestInitialize]
+        public void SetUp()
+        {
+            _connection = new SqliteConnection("DataSource=:memory:");
+            _connection.Open();
+
+            var options = new DbContextOptionsBuilder<BuildingAdminContext>()
+                .UseSqlite(_connection)
+                .Options;
+
+            _context = new BuildingAdminContext(options);
+            _context.Database.EnsureCreated();
+
+            _repository = new ManagerRepository(_context);
+        }
+
+        [TestMethod]
+        public void TestInsert()
+        {
+            Manager manager = new Manager
+            {
+                Id = 1,
+                Name = "Luis Perez",
+                Email = "luis@test.com",
+                Password = "Prueba.1234"
+            };
+
+            _repository.Insert(manager);
+
+            Assert.AreEqual(manager, _context.Managers.Find(0));
+        }
+    }
+}
