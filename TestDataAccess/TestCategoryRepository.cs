e4751fba9dab9b40226e35c4681c3ee774436c83
Agustina Martinez <agustinamartinez1044@gmail.com>
2024-04-23 19:30:35 -0300

    feat(CategoryRepository): setup

---
+++ TestDataAccess/TestCategoryRepository.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Domain;
+using DataAccess;
+using Microsoft.Data.Sqlite;
+using Microsoft.EntityFrameworkCore;
+using System.ComponentModel;
+
+namespace TestDataAccess
+{
+    [TestClass]
+    public class TestCategoryRepository
+    {
+        private BuildingAdminContext _context;
+
+        private SqliteConnection _connection;
+
+        private CategoryRepository _categoryRepository;
+
+        [TestInitialize]
+        public void SetUp()
+        {
+            _connection = new SqliteConnection("DataSource=:memory:");
+            _connection.Open();
+
+            var options = new DbContextOptionsBuilder<BuildingAdminContext>()
+                .UseSqlite(_connection)
+                .Options;
+
+            _context = new BuildingAdminContext(options);
+            _context.Database.EnsureCreated();
+
+            _categoryRepository = new CategoryRepository(_context);
+        }
+
+    }
+}
