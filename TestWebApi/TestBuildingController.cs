e8ae592d4ea4737e813975d2e4cbad74c3ae93bf
rodrigoconze <rodriconze@gmail.com>
2024-04-27 14:45:51 -0300

    feat(BuildingController): [RED] create building with empty location

--- TestWebApi/TestBuildingController.cs
+++ TestWebApi/TestBuildingController.cs
@@ -186,5 +186,25 @@ namespace TestWebApi
 
             Assert.IsTrue(result.GetType().Equals(typeof(BadRequestResult)));
         }
+
+        [TestMethod]
+        public void TestCreateBuildingWithEmptyLocation()
+        {
+            _buildingServices = new Mock<IBuildingServices>(MockBehavior.Strict);
+            var buildingController = new BuildingController(_buildingServices.Object);
+            CreateBuildingInput input = new CreateBuildingInput()
+            {
+                Name = "Edificio nuevo",
+                Address = "Calle, 123, esquina",
+                Location = "",
+                ConstructionCompany = "Empresa constructora",
+                Expenses = 1000,
+                Apartments = new List<NewApartmentInput>()
+            };
+
+            var result = buildingController.CreateBuilding(input);
+
+            Assert.IsTrue(result.GetType().Equals(typeof(BadRequestResult)));
+        }
     }
 }
