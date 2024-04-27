3a9ec1228fca7dd6d4829f7cbf03c6faf020a58b
Sebastian Borjas <sebastianborjas2000@gmail.com>
2024-04-27 18:51:08 -0300

    feat(UserService): [RED] create administrator

--- Services/UserService.cs
+++ Services/UserService.cs
@@ -1,9 +1,22 @@
-﻿namespace Services;
-using IServices;
+﻿using IServices;
 using Domain;
+using IDataAcess;
+
+namespace Services;
 
 public class UserService : IUserServices
 {
+  private readonly IGenericRepository<Administrator> _adminRepository;
+  private readonly IGenericRepository<MaintenanceOperator> _operatorRepository;
+  private readonly IGenericRepository<Manager> _managerRepository;
+
+  public UserService(IGenericRepository<Administrator> adminRepository, IGenericRepository<MaintenanceOperator> operatorRepository, IGenericRepository<Manager> managerRepository)
+  {
+    _adminRepository = adminRepository;
+    _operatorRepository = operatorRepository;
+    _managerRepository = managerRepository;
+  }
+
   public Administrator CreateAdministrator(Administrator administrator)
   {
     return new NotImplementedException();
---
+++ TestServices/TestUserService.cs
@@ -0,0 +1,45 @@
+using Domain;
+using Services;
+using Moq;
+using IDataAcess;
+
+namespace TestServices;
+
+[TestClass]
+public class TestUserService
+{
+  private UserService _service;
+  private Mock<IGenericRepository<Administrator>> _adminRepositoryMock;
+  private Mock<IGenericRepository<MaintenanceOperator>> _operatorRepositoryMock;
+  private Mock<IGenericRepository<Manager>> _managerRepositoryMock;
+
+  [TestInitialize]
+  public void SetUp()
+  {
+    _adminRepositoryMock = new Mock<IGenericRepository<Administrator>>(MockBehavior.Strict);
+    _operatorRepositoryMock = new Mock<IGenericRepository<MaintenanceOperator>>();
+    _managerRepositoryMock = new Mock<IGenericRepository<Manager>>();
+  }
+
+  [TestMethod]
+  public void CreateCorrectAdministrator()
+  {
+    _adminRepositoryMock.Setup(r => r.Insert(It.IsAny<Administrator>())).Verifiable();
+
+    _service = new UserService(_adminRepositoryMock.Object, _operatorRepositoryMock.Object, _managerRepositoryMock.Object);
+
+    var administrator = new Administrator
+    {
+      Name = "Fernando",
+      LastName = "Alonso",
+      Email = "elnano@padre.com",
+      Password = "Nano.1234"
+    };
+
+    var createdAdmin = _service.CreateAdministrator(administrator);
+
+    _adminRepositoryMock.VerifyAll();
+    Assert.AreEqual(administrator, createdAdmin);
+  }
+
+}
