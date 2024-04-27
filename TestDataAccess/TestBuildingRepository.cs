950ad7f4fefe299b05325e2587e9df088e0b4a12
Agustina Martinez <agustinamartinez1044@gmail.com>
2024-04-27 15:11:20 -0300

    feat(TestBuildingRepository): set up test

---
+++ TestDataAccess/TestBuildingRepository.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Data.Sqlite;
+using Microsoft.EntityFrameworkCore;
+using DataAccess;
+using Domain;
+
+namespace TestDataAccess
+{
+    [TestClass]
+    public class TestBuildingRepository
+    {
+        private BuildingAdminContext _context;
+        private SqliteConnection _connection;
+        private BuildingRepository _repository;
+
+        [TestInitialize]
+        public void SetUp()
+        {
+            _connection = new SqliteConnection("DataSource=:memory:");
+            _connection.Open();
+
+            var options = new DbContextOptionsBuilder<BuildingAdminContext>()
+                .UseSqlite(_connection)
+                .Options;
+
+            _context = new BuildingAdminContext(options);
+            _context.Database.EnsureCreated();
+
+            _repository = new BuildingRepository(_context);
+        }
+    }
+}
