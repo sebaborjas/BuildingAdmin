5c928732f43e8a69105bb1bc6bcb6ff106396c34
rodrigoconze <rodriconze@gmail.com>
2024-04-27 14:39:35 -0300

    feat(BuildingController): [RED] create building without address

--- TestWebApi/TestBuildingController.cs
+++ TestWebApi/TestBuildingController.cs
@@ -128,5 +128,24 @@ namespace TestWebApi
 
             Assert.IsTrue(result.GetType().Equals(typeof(BadRequestResult)));
         }
+
+        [TestMethod]
+        public void TestCreateBuildingWithoutAddress()
+        {
+            _buildingServices = new Mock<IBuildingServices>(MockBehavior.Strict);
+            var buildingController = new BuildingController(_buildingServices.Object);
+            CreateBuildingInput input = new CreateBuildingInput()
+            {
+                Name = "Edificio nuevo",
+                Location = "111,111",
+                ConstructionCompany = "Empresa constructora",
+                Expenses = 1000,
+                Apartments = new List<NewApartmentInput>()
+            };
+
+            var result = buildingController.CreateBuilding(input);
+
+            Assert.IsTrue(result.GetType().Equals(typeof(BadRequestResult)));
+        }
     }
 }
