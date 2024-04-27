96714be15fc363361de20deddbddb7d99c9a0742
rodrigoconze <rodriconze@gmail.com>
2024-04-26 23:27:14 -0300

    feat(InvitationController): [REFACTOR] create invitation with empty mail

--- WebApi/Controllers/InvitationController.cs
+++ WebApi/Controllers/InvitationController.cs
@@ -21,7 +21,7 @@ namespace WebApi.Controllers
         [HttpPost]
         public IActionResult CreateInvitation([FromBody]CreateInvitationInput newInvitationInput)
         {
-            if(newInvitationInput == null || newInvitationInput.Email == null || newInvitationInput.Email == "")
+            if(newInvitationInput == null || string.IsNullOrWhiteSpace(newInvitationInput.Email))
             {
                 return BadRequest();
             }
