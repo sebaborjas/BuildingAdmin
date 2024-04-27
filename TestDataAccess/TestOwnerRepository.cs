3e4a93cd725d98cc90a7d04f645a2279d00cbc3e
Agustina Martinez <agustinamartinez1044@gmail.com>
2024-04-26 21:58:22 -0300

    feat(OwnerRepository): set up

---
+++ TestDataAccess/TestOwnerRepository.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Data.Sqlite;
+using Microsoft.EntityFrameworkCore;
+
+using Domain;
+using DataAccess;
+
+namespace TestDataAccess
+{
+    [TestClass]
+    public class TestOwnerRepository
+    {
+        private BuildingAdminContext _context;
+        private SqliteConnection _connection;
+        private OwnerRepository _repository;
+
+        [TestInitialize]
+        public void SetUp()
+        {
+            _connection = new SqliteConnection("DataSource=:memory:");
+            _connection.Open();
+
+            var options = new DbContextOptionsBuilder<BuildingAdminContext>()
+                .UseSqlite(_connection)
+                .Options;
+
+            _context = new BuildingAdminContext(options);
+            _context.Database.EnsureCreated();
+
+            _repository = new OwnerRepository(_context);
+        }
+
+    }
+}
