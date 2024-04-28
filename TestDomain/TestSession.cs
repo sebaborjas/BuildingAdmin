e4ce0b944e10d22bafe80de59b57da7ea8d51e8f
rodrigoconze <rodriconze@gmail.com>
2024-04-28 02:59:10 -0300

    feat(Session): [GREEN] test id

---
+++ TestDomain/TestSession.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Domain;
+
+namespace TestDomain
+{
+    [TestClass]
+    public class TestSession
+    {
+
+        [TestMethod]
+        public void TestId()
+        {
+            var session = new Session();
+            session.Id = 10;
+            Assert.AreEqual(10, session.Id);
+        }
+    }
+}
