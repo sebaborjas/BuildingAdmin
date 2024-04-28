db07876456670e5882db2a9898d729bc0d8a2d98
Agustina Martinez <agustinamartinez1044@gmail.com>
2024-04-27 21:14:47 -0300

    feat(TicketController): add model create

---
+++ DTO/In/TicketCreateModel.cs
@@ -0,0 +1,27 @@
+﻿using Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DTO.In
+{
+    public class TicketCreateModel
+    {
+        public string Description { get; set; }
+        public Apartment Apartment { get; set; }
+        public Category Category { get; set; }
+
+        public Ticket ToEntity()
+        {
+            return new Ticket()
+            {
+                Description = Description,
+                Apartment = Apartment,
+                Category = Category
+            };
+        }
+
+    }
+}
