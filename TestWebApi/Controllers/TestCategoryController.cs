2afbf27a2998e27e07ac2da40d6e398c0ee02adb
Agustina Martinez <agustinamartinez1044@gmail.com>
2024-04-28 16:47:53 -0300

    feat(CategoryController): [GREEN] create category

---
+++ TestWebApi/Controllers/TestCategoryController.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Domain;
+using IServices;
+using WebApi;
+using Moq;
+using DTO.In;
+using Microsoft.AspNetCore.Mvc;
+using DTO.Out;
+
+namespace TestWebApi;
+
+[TestClass]
+public class TestCategoryController
+{
+    private Mock<ICategoryService> _categoryServiceMock;
+
+    [TestInitialize]
+    public void SetUp()
+    {
+        _categoryServiceMock = new Mock<ICategoryService>(MockBehavior.Strict);
+    }
+
+    [TestMethod]
+    public void TestCreateCategory()
+    {
+
+        Category category = new Category()
+        {
+            Name = "Test"
+        };
+
+        var createCategoryModel = new CreateCategoryModel()
+        {
+            Name = "Test"
+        };
+
+        _categoryServiceMock.Setup(x => x.CreateCategory(It.IsAny<string>())).Returns(category);
+        var result = _categoryServiceMock.Object;
+        Assert.IsNotNull(result);
+
+        var controller = new CategoryController(_categoryServiceMock.Object);
+        var response = controller.CreateCategory(createCategoryModel) as OkResult;
+        Assert.IsNotNull(response);
+
+        _categoryServiceMock.Verify(x => x.CreateCategory(It.IsAny<string>()), Times.Once);
+    }
+    
+}
---
+++ WebApi/Controllers/CategoryController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using IServices;
+using DTO.In;
+
+namespace WebApi
+{
+    [Route("api/categories")]
+    [ApiController]
+    public class CategoryController : ControllerBase
+    {
+        private readonly ICategoryService _service;
+
+        public CategoryController(ICategoryService service)
+        {
+            _service = service;
+        }
+
+        [HttpPost]
+        public IActionResult CreateCategory([FromBody] CreateCategoryModel createCategoryModel)
+        {
+            if (!IsValidCreateCategoryInput(createCategoryModel))
+            {
+                return BadRequest();
+            }
+            _service.CreateCategory(createCategoryModel.Name);
+            return Ok();
+        }
+
+        private bool IsValidCreateCategoryInput(CreateCategoryModel createCategoryModel)
+        {
+            return createCategoryModel != null && !string.IsNullOrEmpty(createCategoryModel.Name);
+        }
+    }
+}
