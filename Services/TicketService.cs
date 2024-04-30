0f00ff9ad1d74435e9c27ef99fce7d82faae9108
rodrigoconze <rodriconze@gmail.com>
2024-04-30 01:44:03 -0300

    feat(TicketService): create ticket

---
+++ Services/TicketService.cs
@@ -0,0 +1,50 @@
+﻿using Domain;
+using IDataAcess;
+using IServices;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Services
+{
+    public class TicketService : ITicketService
+    {
+        private IGenericRepository<Ticket> _ticketRepository;
+        private ISessionService _sessionService;
+
+        public TicketService(IGenericRepository<Ticket> repository, ISessionService sessionService)
+        {
+            _ticketRepository = repository;
+            _sessionService = sessionService;
+        }
+
+        public Ticket CreateTicket(Ticket ticket)
+        {
+            var currentUser = _sessionService.GetCurrentUser();
+            _ticketRepository.Insert(ticket);
+            return ticket;
+        }
+
+        public Ticket AssignTicket(int id, int maintenanceOperatorId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Ticket CompleteTicket(int id, float totalCost)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Ticket> GetTickets(string category = null)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Ticket StartTicket(int id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ TestServices/TestTicketService.cs
@@ -0,0 +1,57 @@
+﻿using Domain;
+using IDataAcess;
+using IServices;
+using Moq;
+using Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestServices
+{
+    [TestClass]
+    public class TestTicketService
+    {
+        private ITicketService _ticketService;
+        private Mock<IGenericRepository<Ticket>> _ticketRepository;
+        private Mock<ISessionService> _sessionService;
+
+        [TestInitialize]
+        public void Setup()
+        {
+            _ticketRepository = new Mock<IGenericRepository<Ticket>>(MockBehavior.Strict);
+            _sessionService = new Mock<ISessionService>(MockBehavior.Strict);
+        }
+
+        [TestMethod]
+        public void TestCreateTicket()
+        {
+            var user = new Manager()
+            {
+                Buildings = [],
+                Email = "manager@correo.com",
+                Id = 1,
+                Name = "Lucas",
+                LastName = "Gonzalez",
+                Password = "Lu.Go123!",
+            };
+            var ticket = new Ticket()
+            {
+                Category = new Category(),
+                Apartment = new Apartment(),
+                Description = "Perdida de agua",
+            };
+            _sessionService.Setup(r=>r.GetCurrentUser(It.IsAny<Guid?>())).Returns(user);
+            _ticketRepository.Setup(r => r.Insert(It.IsAny<Ticket>())).Verifiable();
+            _ticketService = new TicketService(_ticketRepository.Object, _sessionService.Object);
+
+            var result = _ticketService.CreateTicket(ticket);
+
+            _sessionService.VerifyAll();
+            _ticketRepository.VerifyAll();
+            Assert.AreEqual(result, ticket);
+        }
+    }
+}
