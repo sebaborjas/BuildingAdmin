f0f163cb529640ae338cf1e5ee1cadd219c65bb1
Agustina Martinez <agustinamartinez1044@gmail.com>
2024-04-30 01:50:44 -0300

    feat(InvitationService): [RED] create invitation

--- TestServices/TestInvitationService.cs
+++ TestServices/TestInvitationService.cs
@@ -3,11 +3,47 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using IDataAcess;
+using Services;
+using Domain;
+using Moq;
+using IServices;
+using System.Linq.Expressions;
 
 namespace TestServices
 {
     [TestClass]
     public class TestInvitationService
     {
+        private Mock<IGenericRepository<Invitation>> _invitationRepositoryMock;
+        private InvitationService _service;
+
+        [TestInitialize]
+        public void setUp()
+        {
+            _invitationRepositoryMock = new Mock<IGenericRepository<Invitation>>(MockBehavior.Strict);
+        }
+
+        [TestMethod]
+        public void TestCreateInvitation()
+        {
+            _invitationRepositoryMock.Setup(r => r.GetByCondition(It.IsAny<Expression<Func<Invitation, bool>>>(), It.IsAny<List<string>>())).Returns((Expression<Func<Invitation, bool>> predicate, List<string> includes) => null);
+
+            _invitationRepositoryMock.Setup(r => r.Insert(It.IsAny<Invitation>())).Verifiable();
+
+            _service = new InvitationService(_invitationRepositoryMock.Object);
+
+            var invitation = new Invitation
+            {
+                Email = "test@tes.com",
+                ExpirationDate = DateTime.Now.AddDays(3),
+                Name = "Test",
+            };
+
+            var invitationModel = _service.CreateInvitation(invitation);
+
+            _invitationRepositoryMock.VerifyAll();
+            Assert.AreEqual(invitation, invitationModel);
+        }
     }
 }
