15c4f455fbad6eeb0a0d191f0dbedc0cbe92ec9b
rodrigoconze <rodriconze@gmail.com>
2024-04-29 21:04:21 -0300

    feat(ReportController): setup report controller tests

---
+++ TestWebApi/TestReportController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Domain;
+using DTO.Out;
+using IServices;
+using Microsoft.AspNetCore.Mvc;
+using Moq;
+using WebApi.Controllers;
+
+namespace TestWebApi
+{
+    [TestClass]
+    public class TestReportController
+    {
+
+        private Mock<IReportServices> _reportServices;
+
+        [TestMethod]
+        public void TestGetRequestsByBuildingReportForAll()
+        {
+
+        }
+    }
+}
---
+++ WebApi/Controllers/ReportController.cs
@@ -0,0 +1,18 @@
+﻿using IServices;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApi.Controllers
+{
+    [Route("api/reports")]
+    [ApiController]
+    public class ReportController : ControllerBase
+    {
+        private readonly IReportServices _reportServices;
+
+        public ReportController(IReportServices reportServices)
+        {
+            _reportServices = reportServices;
+        }
+    }
+}
