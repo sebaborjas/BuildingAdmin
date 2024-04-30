93872fb59e134421683f6f08ff6239d41a02d74a
rodrigoconze <rodriconze@gmail.com>
2024-04-30 02:28:43 -0300

    feat(TicketService): create ticket with invalid category

--- Services/TicketService.cs
+++ Services/TicketService.cs
@@ -12,17 +12,24 @@ namespace Services
     public class TicketService : ITicketService
     {
         private IGenericRepository<Ticket> _ticketRepository;
+        private IGenericRepository<Category> _categoryRepository;
         private ISessionService _sessionService;
 
-        public TicketService(IGenericRepository<Ticket> repository, ISessionService sessionService)
+        public TicketService(IGenericRepository<Ticket> repository, ISessionService sessionService, IGenericRepository<Category> categoryRepository)
         {
             _ticketRepository = repository;
             _sessionService = sessionService;
+            _categoryRepository = categoryRepository;
         }
 
         public Ticket CreateTicket(Ticket ticket)
         {
             var currentUser = _sessionService.GetCurrentUser();
+            var category = _categoryRepository.Get(ticket.Category.Id);
+            if(category == null )
+            {
+                throw new InvalidDataException("Invalid category id");
+            };
             ticket.CreatedBy = currentUser;
             _ticketRepository.Insert(ticket);
             return ticket;
--- TestServices/TestTicketService.cs
+++ TestServices/TestTicketService.cs
@@ -16,19 +16,32 @@ namespace TestServices
     {
         private ITicketService _ticketService;
         private Mock<IGenericRepository<Ticket>> _ticketRepository;
+        private Mock<IGenericRepository<Building>> _buildingRepository;
+        private Mock<IGenericRepository<Apartment>> _apartmentRepository;
+        private Mock<IGenericRepository<Category>> _categoryRepository;
         private Mock<ISessionService> _sessionService;
+        
+        private Manager _user;
+        private Building _building;
+        private Apartment _apartment;
+        private Category _category;
 
         [TestInitialize]
         public void Setup()
         {
             _ticketRepository = new Mock<IGenericRepository<Ticket>>(MockBehavior.Strict);
+            _buildingRepository = new Mock<IGenericRepository<Building>>(MockBehavior.Strict);
+            _apartmentRepository = new Mock<IGenericRepository<Apartment>>(MockBehavior.Strict);
+            _categoryRepository = new Mock<IGenericRepository<Category>>(MockBehavior.Strict);
             _sessionService = new Mock<ISessionService>(MockBehavior.Strict);
-        }
 
-        [TestMethod]
-        public void TestCreateTicket()
-        {
-            var user = new Manager()
+            _category = new Category()
+            {
+                Id = 1,
+                Name = "Sanitaria"
+            };
+
+            _user = new Manager()
             {
                 Buildings = [],
                 Email = "manager@correo.com",
@@ -37,15 +50,44 @@ namespace TestServices
                 LastName = "Gonzalez",
                 Password = "Lu.Go123!",
             };
+
+            _apartment = new Apartment()
+            {
+                Id = 1,
+                Bathrooms = 2,
+                DoorNumber = 3,
+                Floor = 1,
+                HasTerrace = false,
+                Owner = new Owner(),
+                Rooms = 2
+            };
+
+            _building = new Building()
+            {
+                Id = 1,
+                Apartments = [_apartment],
+                Address = "Rivera, 1111, Soca",
+                ConstructionCompany = new ConstructionCompany(),
+                Expenses = 2000,
+                Location = "111,111",
+                Name = "Edificio Las Delicias",
+            };
+
+        }
+
+        [TestMethod]
+        public void TestCreateTicket()
+        {
             var ticket = new Ticket()
             {
-                Category = new Category(),
-                Apartment = new Apartment(),
+                Category = new Category() { Id = 1 },
+                Apartment = new Apartment() { Id = 1 },
                 Description = "Perdida de agua",
             };
-            _sessionService.Setup(r=>r.GetCurrentUser(It.IsAny<Guid?>())).Returns(user);
+            _sessionService.Setup(r=>r.GetCurrentUser(It.IsAny<Guid?>())).Returns(_user);
             _ticketRepository.Setup(r => r.Insert(It.IsAny<Ticket>())).Verifiable();
-            _ticketService = new TicketService(_ticketRepository.Object, _sessionService.Object);
+            _categoryRepository.Setup(r => r.Get(It.IsAny<int>())).Returns(_category);
+            _ticketService = new TicketService(_ticketRepository.Object, _sessionService.Object, _categoryRepository.Object);
 
             var result = _ticketService.CreateTicket(ticket);
 
@@ -57,30 +99,40 @@ namespace TestServices
         [TestMethod]
         public void TestTicketCreatedBy()
         {
-            var user = new Manager()
-            {
-                Buildings = [],
-                Email = "manager@correo.com",
-                Id = 1,
-                Name = "Lucas",
-                LastName = "Gonzalez",
-                Password = "Lu.Go123!",
-            };
             var ticket = new Ticket()
             {
-                Category = new Category(),
-                Apartment = new Apartment(),
+                Category = new Category() { Id = 1 },
+                Apartment = new Apartment() { Id = 1 },
                 Description = "Perdida de agua",
             };
-            _sessionService.Setup(r => r.GetCurrentUser(It.IsAny<Guid?>())).Returns(user);
+            _sessionService.Setup(r => r.GetCurrentUser(It.IsAny<Guid?>())).Returns(_user);
             _ticketRepository.Setup(r => r.Insert(It.IsAny<Ticket>())).Verifiable();
-            _ticketService = new TicketService(_ticketRepository.Object, _sessionService.Object);
+            _categoryRepository.Setup(r => r.Get(It.IsAny<int>())).Returns(_category);
+            _ticketService = new TicketService(_ticketRepository.Object, _sessionService.Object, _categoryRepository.Object);
 
             var result = _ticketService.CreateTicket(ticket);
 
             _sessionService.VerifyAll();
             _ticketRepository.VerifyAll();
-            Assert.AreEqual(result.CreatedBy, user);
+            Assert.AreEqual(result.CreatedBy, _user);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(InvalidDataException))]
+        public void TestCreateTicketWithInvalidCategory()
+        {
+            var ticket = new Ticket()
+            {
+                Category = new Category() { Id = 2 },
+                Apartment = new Apartment() { Id = 1 },
+                Description = "Perdida de agua",
+            };
+            Category nullCategory = null;
+            _sessionService.Setup(r => r.GetCurrentUser(It.IsAny<Guid?>())).Returns(_user);
+            _categoryRepository.Setup(r => r.Get(It.IsAny<int>())).Returns(nullCategory);
+            _ticketService = new TicketService(_ticketRepository.Object, _sessionService.Object, _categoryRepository.Object);
+
+            _ticketService.CreateTicket(ticket);
         }
     }
 }
