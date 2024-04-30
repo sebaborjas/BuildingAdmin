28456f8b7a7bbd51c0eb3819519a7eabb70fc1b6
Agustina Martinez <agustinamartinez1044@gmail.com>
2024-04-29 22:42:41 -0300

    feat(TestCategoryServices): create category white space

--- TestServices/TestCategoryService.cs
+++ TestServices/TestCategoryService.cs
@@ -62,6 +62,20 @@ namespace TestServices
 
             Assert.AreEqual("", category.Name);
         }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void TestCreateCategoryWithWhitespaceName()
+        {
+            _service = new CategoryService(_categoryRepositoryMock.Object);
+            _categoryRepositoryMock.Setup(r => r.Insert(It.IsAny<Category>())).Verifiable();
+
+            var category = _service.CreateCategory("     ");
+            Assert.IsNotNull(category);
+            _categoryRepositoryMock.VerifyAll();
+
+            Assert.AreEqual("     ", category.Name);
+        }
     }
 
 }
