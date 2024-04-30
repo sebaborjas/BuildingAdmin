ffeff607dfea7f140c50638f2a9b74d377c70280
Agustina Martinez <agustinamartinez1044@gmail.com>
2024-04-29 22:37:24 -0300

    feat(TestCategoryServices): create category

--- Services/CategoryService.cs
+++ Services/CategoryService.cs
@@ -3,10 +3,27 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using IServices;
+using IDataAcess;
+using Domain;
 
 namespace Services
 {
-    public class CategoryService
+    public class CategoryService : ICategoryService
     {
+        public readonly IGenericRepository<Category> _categoryRepository;
+
+        public CategoryService(IGenericRepository<Category> categoryRepository)
+        {
+            _categoryRepository = categoryRepository;
+        }
+
+        public Category CreateCategory(string name)
+        {
+            Category category = new Category();
+            category.Name = name;
+            _categoryRepository.Insert(category);
+            return category;
+        }
     }
 }
--- TestServices/TestCategoryService.cs
+++ TestServices/TestCategoryService.cs
@@ -1,12 +1,38 @@
-﻿using System;
+﻿using Moq;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using IDataAcess;
+using Services;
+using Domain;
 
 namespace TestServices
 {
+    [TestClass]
     public class TestCategoryService
     {
+        private CategoryService _service;
+        private Mock<IGenericRepository<Category>> _categoryRepositoryMock;
+
+        [TestInitialize]
+        public void SetUp()
+        {
+            _categoryRepositoryMock = new Mock<IGenericRepository<Category>>();
+        }
+
+        [TestMethod]
+        public void TestCreateCategory()
+        {
+            _service = new CategoryService(_categoryRepositoryMock.Object);
+            _categoryRepositoryMock.Setup(r => r.Insert(It.IsAny<Category>())).Verifiable();
+
+            var category = _service.CreateCategory("Test");
+            Assert.IsNotNull(category);
+            _categoryRepositoryMock.VerifyAll();
+
+            Assert.AreEqual("Test", category.Name);
+        }
     }
 }
