9ad0fd0441db3d94bcb1b8154703ddba956d1804
Agustina Martinez <agustinamartinez1044@gmail.com>
2024-04-30 00:36:59 -0300

    feat(InvitationServices): set up class

---
+++ TestServices/TestInvitationService.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestServices
+{
+    [TestClass]
+    public class TestInvitationService
+    {
+    }
+}
