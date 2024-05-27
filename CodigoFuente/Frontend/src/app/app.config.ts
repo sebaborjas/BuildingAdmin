72cd3e1a5be7397e934fc97f9c553bbd4da74afb
Sebastian Borjas <sebastianborjas2000@gmail.com>
2024-05-27 02:09:19 -0300

    feat(WebUI/login): add services & interceptors to app config

--- CodigoFuente/Frontend/src/app/app.config.ts
+++ CodigoFuente/Frontend/src/app/app.config.ts
@@ -2,7 +2,22 @@ import { ApplicationConfig } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
+import { AuthService } from './services/auth.service';
+import { SesionStorageService } from './services/sesion-storage.service';
+import { provideHttpClient } from '@angular/common/http';
+import { HTTP_INTERCEPTORS } from '@angular/common/http';
+import { AuthInterceptor } from './Interceptors/auth.interceptor';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes)]
+  providers: [
+    provideRouter(routes),
+    provideHttpClient(),
+    AuthService,
+    SesionStorageService,
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: AuthInterceptor,
+      multi: true
+    }
+  ]
 };
