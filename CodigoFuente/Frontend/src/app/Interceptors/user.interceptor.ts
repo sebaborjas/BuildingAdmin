587fe284e01d5a4d79dd9f3299726219ccce7955
Sebastian Borjas <sebastianborjas2000@gmail.com>
2024-05-29 22:43:40 -0300

    feat(WebUI/User): add user service ask bakend for user session

---
+++ CodigoFuente/Frontend/src/app/Interceptors/user.interceptor.ts
@@ -0,0 +1,31 @@
+import {
+  HttpRequest,
+  HttpHandler,
+  HttpEvent,
+  HttpInterceptor,
+} from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { SesionStorageService } from '../services/sesion-storage.service';
+import { Observable } from 'rxjs';
+
+@Injectable()
+export class UserInterceptor implements HttpInterceptor {
+  constructor(private _sesionStorqageService: SesionStorageService) {}
+
+  intercept(
+    req: HttpRequest<unknown>,
+    next: HttpHandler
+  ): Observable<HttpEvent<unknown>> {
+    const token = this._sesionStorqageService.getToken();
+    let newRequest = req;
+    if (req.url.includes('users')) {
+      return next.handle(req);
+    }
+
+    if (token) {
+      const headers = req.headers.set('Authorization', token);
+      newRequest = req.clone({ headers });
+    }
+    return next.handle(newRequest);
+  }
+}
--- CodigoFuente/Frontend/src/networking/endpoints.ts
+++ CodigoFuente/Frontend/src/networking/endpoints.ts
@@ -1,3 +1,7 @@
 export enum LoginEndpoint {
   LOGIN = '/api/v2/login',
 }
+
+export enum UserEndpoint {
+  USER = '/api/v2/users',
+}
