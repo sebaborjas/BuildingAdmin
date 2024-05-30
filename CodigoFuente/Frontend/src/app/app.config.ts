284f62272990bd2be3c49952fdf670e64358fc74
Sebastian Borjas <sebastianborjas2000@gmail.com>
2024-05-29 22:44:51 -0300

    feat(WebUI): add user service & interceptor config

--- CodigoFuente/Frontend/src/app/app.config.ts
+++ CodigoFuente/Frontend/src/app/app.config.ts
@@ -7,17 +7,24 @@ import { SesionStorageService } from './services/sesion-storage.service';
 import { provideHttpClient } from '@angular/common/http';
 import { HTTP_INTERCEPTORS } from '@angular/common/http';
 import { AuthInterceptor } from './Interceptors/auth.interceptor';
+import { UserInterceptor } from './Interceptors/user.interceptor';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideRouter(routes),
     provideHttpClient(),
     AuthService,
+    UserInterceptor,
     SesionStorageService,
     {
       provide: HTTP_INTERCEPTORS,
       useClass: AuthInterceptor,
-      multi: true
-    }
-  ]
+      multi: true,
+    },
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: UserInterceptor,
+      multi: true,
+    },
+  ],
 };
