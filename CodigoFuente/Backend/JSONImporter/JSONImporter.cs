f5957f1e0c6a7c95832449b545454df28caffc63
Agustina Martinez <agustinamartinez1044@gmail.com>
2024-06-01 16:05:48 -0300

    feat(Reflection): implement json importer

---
+++ CodigoFuente/Backend/JSONImporter/JSONImporter.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using DTO.In;
+using IImporter;
+using Newtonsoft.Json;
+
+namespace JSONImporter
+{
+    public class JSONFileImporter : ImporterInterface
+    {
+        public string GetName()
+        {
+            return "JSON";
+        }
+
+        public List<CreateBuildingInput> ImportFile(ImporterInput input)
+        {
+            var jsonFile = File.ReadAllText(input.Path);
+            var buildings = JsonConvert.DeserializeObject<List<CreateBuildingInput>>(jsonFile);
+
+            return buildings;
+        }
+    }
+}
