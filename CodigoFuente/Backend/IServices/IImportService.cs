c2419b800b46c5722b92caab364c23c4ae780710
Agustina Martinez <agustinamartinez1044@gmail.com>
2024-06-01 16:04:31 -0300

    feat(Reflection): importer interface

---
+++ CodigoFuente/Backend/IServices/IImportService.cs
@@ -0,0 +1,12 @@
+﻿using IImporter;
+using Domain;
+
+namespace IServices
+{
+    public interface IImportService
+    {
+        List<ImporterInterface> GetAllImporters();
+
+        List<Building> ImportBuildings(string importerName, string path);
+    }
+}
