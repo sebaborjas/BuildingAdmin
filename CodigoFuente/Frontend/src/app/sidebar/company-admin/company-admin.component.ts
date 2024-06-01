10fdd4385fdbad52a1da7d665139e9610c2b6eaa
Sebastian Borjas <sebastianborjas2000@gmail.com>
2024-06-01 18:30:04 -0300

    feat: add company admin component section

---
+++ CodigoFuente/Frontend/src/app/sidebar/company-admin/company-admin.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-company-admin',
+  standalone: true,
+  imports: [],
+  templateUrl: './company-admin.component.html',
+  styleUrl: './company-admin.component.css'
+})
+export class CompanyAdminComponent {
+
+}
