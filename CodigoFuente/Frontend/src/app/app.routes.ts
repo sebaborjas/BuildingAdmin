d93815fbbf22e6327915129d1bfa5068f77b949b
Sebastian Borjas <sebastianborjas2000@gmail.com>
2024-06-02 18:08:43 -0300

    feat(routes): add dashboard to home child

--- CodigoFuente/Frontend/src/app/app.routes.ts
+++ CodigoFuente/Frontend/src/app/app.routes.ts
@@ -2,13 +2,17 @@ import { Routes } from '@angular/router';
 import { LoginComponent } from './login/login.component';
 import { HomeComponent } from './home/home.component';
 import { InvitationsComponent } from './pages/invitations/invitations.component';
+import { DashboardComponent } from './pages/dashboard/dashboard.component';
 
 export const routes: Routes = [
   { path: 'login', component: LoginComponent },
   {
     path: 'home',
     component: HomeComponent,
-    children: [{ path: 'invitations', component: InvitationsComponent }],
+    children: [
+      { path: 'invitations', component: InvitationsComponent },
+      { path: 'dashboard', component: DashboardComponent },
+    ],
   },
   { path: '**', component: LoginComponent },
 ];
