33dbfd4535affbca609e9cc3f0dcde560f18df93
Sebastian Borjas <sebastianborjas2000@gmail.com>
2024-06-02 17:58:14 -0300

    feat(sidebar): add navigate function to admin sections

--- CodigoFuente/Frontend/src/app/sidebar/admin/admin.component.ts
+++ CodigoFuente/Frontend/src/app/sidebar/admin/admin.component.ts
@@ -1,5 +1,6 @@
-import { Component, INJECTOR, Input } from '@angular/core';
+import { Component } from '@angular/core';
 import { SectionComponent } from '../section/section.component';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-admin',
@@ -9,11 +10,15 @@ import { SectionComponent } from '../section/section.component';
   styleUrl: './admin.component.css',
 })
 export class AdminComponent {
-  constructor() {}
+  constructor(private _router: Router) {}
 
   selectedSectionIndex: number = -1;
 
   selectSection(index: number) {
     this.selectedSectionIndex = index;
+  }
+
+  navigateToSection(section: string) {
+    this._router.navigate(['home', section]);
   }
 }
