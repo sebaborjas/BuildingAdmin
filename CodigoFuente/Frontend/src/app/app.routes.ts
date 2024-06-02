5cdf913399fe82973377f47f8ee713a4e681bcb3
Sebastian Borjas <sebastianborjas2000@gmail.com>
2024-06-02 17:48:07 -0300

    feat(routes): add /invitations child to home route

--- CodigoFuente/Frontend/src/app/app.routes.ts
+++ CodigoFuente/Frontend/src/app/app.routes.ts
@@ -1,9 +1,14 @@
 import { Routes } from '@angular/router';
 import { LoginComponent } from './login/login.component';
 import { HomeComponent } from './home/home.component';
+import { InvitationsComponent } from './pages/invitations/invitations.component';
 
 export const routes: Routes = [
   { path: 'login', component: LoginComponent },
-  { path: 'home', component: HomeComponent },
+  {
+    path: 'home',
+    component: HomeComponent,
+    children: [{ path: 'invitations', component: InvitationsComponent }],
+  },
   { path: '**', component: LoginComponent },
 ];
