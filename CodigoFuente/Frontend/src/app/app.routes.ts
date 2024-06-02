d8467afafae6b9af878b6660ff020cfe0e06016c
Sebastian Borjas <sebastianborjas2000@gmail.com>
2024-06-02 18:14:04 -0300

    feat(routes): add reports to home child

--- CodigoFuente/Frontend/src/app/app.routes.ts
+++ CodigoFuente/Frontend/src/app/app.routes.ts
@@ -3,6 +3,7 @@ import { LoginComponent } from './login/login.component';
 import { HomeComponent } from './home/home.component';
 import { InvitationsComponent } from './pages/invitations/invitations.component';
 import { DashboardComponent } from './pages/dashboard/dashboard.component';
+import { ReportsComponent } from './pages/reports/reports.component';
 
 export const routes: Routes = [
   { path: 'login', component: LoginComponent },
@@ -12,6 +13,7 @@ export const routes: Routes = [
     children: [
       { path: 'invitations', component: InvitationsComponent },
       { path: 'dashboard', component: DashboardComponent },
+      { path: 'reports', component: ReportsComponent },
     ],
   },
   { path: '**', component: LoginComponent },
