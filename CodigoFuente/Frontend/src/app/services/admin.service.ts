e3473498785999405ea18f8e19dc83837e6ba743
Sebastian Borjas <sebastianborjas2000@gmail.com>
2024-06-03 00:57:09 -0300

    feat(services): add admin service with createInvitation method

---
+++ CodigoFuente/Frontend/src/app/services/admin.service.ts
@@ -0,0 +1,31 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { InvitationEndpoint } from '../../networking/endpoints';
+import { Observable } from 'rxjs';
+import { InvitationModel } from './types';
+import { environment } from '../../environments/environment.development';
+
+const BASE_URL = environment.API_URL;
+
+@Injectable({
+  providedIn: 'root',
+})
+export class AdminService {
+  constructor(private _httpClient: HttpClient) {}
+
+  createInvitation(
+    name: string,
+    email: string,
+    expirationDate: string
+  ): Observable<InvitationModel> {
+    const body = {
+      email: email,
+      name: name,
+      expirationDate: expirationDate,
+    };
+    return this._httpClient.post<InvitationModel>(
+      `${BASE_URL}${InvitationEndpoint.INVITATIONS}`,
+      body
+    );
+  }
+}
