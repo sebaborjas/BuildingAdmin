b64e97f465882bb7bb72d4fc9ca4650a097c7bd7
Sebastian Borjas <sebastianborjas2000@gmail.com>
2024-06-03 00:59:40 -0300

    feat(config): import dependencies to realize http request to invitations

--- CodigoFuente/Frontend/src/app/app.config.ts
+++ CodigoFuente/Frontend/src/app/app.config.ts
@@ -4,18 +4,26 @@ import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
 import { AuthService } from './services/auth.service';
 import { SesionStorageService } from './services/sesion-storage.service';
-import { provideHttpClient } from '@angular/common/http';
+import { LoadingService } from './services/loading.service';
+import { AdminService } from './services/admin.service';
+import {
+  provideHttpClient,
+  withInterceptorsFromDi,
+} from '@angular/common/http';
 import { HTTP_INTERCEPTORS } from '@angular/common/http';
 import { AuthInterceptor } from './Interceptors/auth.interceptor';
 import { UserInterceptor } from './Interceptors/user.interceptor';
+import { AdminInterceptor } from './Interceptors/admin.interceptor';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideRouter(routes),
-    provideHttpClient(),
+    provideHttpClient(withInterceptorsFromDi()),
     AuthService,
     UserInterceptor,
     SesionStorageService,
+    LoadingService,
+    AdminService,
     {
       provide: HTTP_INTERCEPTORS,
       useClass: AuthInterceptor,
@@ -24,6 +32,11 @@ export const appConfig: ApplicationConfig = {
     {
       provide: HTTP_INTERCEPTORS,
       useClass: UserInterceptor,
+      multi: true,
+    },
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: AdminInterceptor,
       multi: true,
     },
   ],
