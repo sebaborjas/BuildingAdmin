7b562db9f94f547f83ff729293545ef6df1896a1
Sebastian Borjas <sebastianborjas2000@gmail.com>
2024-06-05 22:36:16 -0300

    feat(Backend): update webui with develop changes

--- CodigoFuente/Backend/WebApi/Controllers/InvitationController.cs
+++ CodigoFuente/Backend/WebApi/Controllers/InvitationController.cs
@@ -14,7 +14,7 @@ namespace WebApi.Controllers
     {
 
         private IInvitationService _invitationService;
-        
+
         public InvitationController(IInvitationService invitationService)
         {
             _invitationService = invitationService;
@@ -22,12 +22,8 @@ namespace WebApi.Controllers
 
         [HttpPost]
         [AuthenticationFilter(Role = RoleConstants.AdministratorRole)]
-        public IActionResult CreateInvitation([FromBody]CreateInvitationInput newInvitationInput)
+        public IActionResult CreateInvitation([FromBody] CreateInvitationInput newInvitationInput)
         {
-            if(!IsValidCreateInvitationInput(newInvitationInput))
-            {
-                return BadRequest("Invalid input");
-            }
             var newInvitation = _invitationService.CreateInvitation(newInvitationInput.ToEntity());
             var response = new CreateInvitationOutput(newInvitation);
             return Ok(response);
@@ -37,90 +33,31 @@ namespace WebApi.Controllers
         [AuthenticationFilter(Role = RoleConstants.AdministratorRole)]
         public IActionResult DeleteInvitation(int id)
         {
-            try
-            {
-                _invitationService.DeleteInvitation(id);
-                return Ok();
-            }
-            catch (Exception exception)
-            {
-                return NotFound();
-            }
+            _invitationService.DeleteInvitation(id);
+            return Ok();
         }
 
         [HttpPut("{id}")]
         [AuthenticationFilter(Role = RoleConstants.AdministratorRole)]
-        public IActionResult ModifyInvitation(int id, [FromBody]ModifyInvitationInput modifyInvitationInput)
+        public IActionResult ModifyInvitation(int id, [FromBody] ModifyInvitationInput modifyInvitationInput)
         {
-            if(!IsValidModifyInvitationInput(modifyInvitationInput))
-            {
-                return BadRequest();
-            }
-            try
-            {
-                _invitationService.ModifyInvitation(id, modifyInvitationInput.ExpirationDate);
-                return Ok();
-            } catch (Exception exception)
-            {
-                return NotFound(exception.Message);
-            }
+            _invitationService.ModifyInvitation(id, modifyInvitationInput.ExpirationDate);
+            return Ok();
         }
 
         [HttpPut("accept")]
         public IActionResult AcceptInvitation(AcceptInvitationInput acceptInvitationInput)
         {
-            if(!IsValidAcceptInvitationInput(acceptInvitationInput))
-            {
-                return BadRequest();
-            }
-            try
-            {
-                var newManager = _invitationService.AcceptInvitation(acceptInvitationInput.ToEntity(), acceptInvitationInput.Password);
-                var result = new AcceptInvitationOutput(newManager);
-                return Ok(result);
-            } catch (Exception exception)
-            {
-                return NotFound(exception.Message);
-            }
+            var newManager = _invitationService.AcceptInvitation(acceptInvitationInput.ToEntity(), acceptInvitationInput.Password);
+            var result = new AcceptInvitationOutput(newManager);
+            return Ok(result);
         }
 
         [HttpPut("reject")]
         public IActionResult RejectInvitation(RejectInvitationInput rejectInvitationInput)
         {
-            if (!IsValidRejectInvitationInput(rejectInvitationInput))
-            {
-                return BadRequest();
-            }
-            try
-            {
-                _invitationService.RejectInvitation(rejectInvitationInput.Email);
-                return Ok();
-            }
-            catch (Exception exception)
-            {
-                return NotFound(exception.Message);
-            }
-            
-        }
-
-        private bool IsValidCreateInvitationInput(CreateInvitationInput newInvitationInput)
-        {
-            return newInvitationInput != null && !string.IsNullOrWhiteSpace(newInvitationInput.Email) && !string.IsNullOrWhiteSpace(newInvitationInput.Name) && newInvitationInput.ExpirationDate > DateTime.Now;
-        }
-
-        private bool IsValidModifyInvitationInput(ModifyInvitationInput modifyInvitationInput)
-        {
-            return modifyInvitationInput != null && modifyInvitationInput.ExpirationDate > DateTime.Now;
-        }
-
-        private bool IsValidAcceptInvitationInput(AcceptInvitationInput acceptInvitationInput)
-        {
-            return acceptInvitationInput != null && !string.IsNullOrWhiteSpace(acceptInvitationInput.Email) && !string.IsNullOrWhiteSpace(acceptInvitationInput.Password);
-        }
-
-        private bool IsValidRejectInvitationInput(RejectInvitationInput rejectInvitationInput)
-        {
-            return rejectInvitationInput != null && !string.IsNullOrWhiteSpace(rejectInvitationInput.Email);
+            _invitationService.RejectInvitation(rejectInvitationInput.Email);
+            return Ok();
         }
 
         [HttpGet]
