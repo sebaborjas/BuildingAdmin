89c2d4f2a3718a8b0ee4f70229c518a771caf707
Agustina Martinez <agustinamartinez1044@gmail.com>
2024-06-08 17:39:29 -0300

    feat(types): interface for tickets

--- CodigoFuente/Frontend/src/app/services/types.ts
+++ CodigoFuente/Frontend/src/app/services/types.ts
@@ -14,3 +14,32 @@ export interface InvitationModel {
   role: number;
   expirationDate: string;
 }
+
+export interface Category {
+  name: string;
+  id: number;
+  relatedTo: string | null;
+}
+
+export interface AssignedTo {
+  lastName: string;
+  buildings: any | null;
+  id: number;
+  name: string;
+  email: string;
+  password: string;
+}
+
+export interface TicketModel {
+  id: number;
+  description: string;
+  creationDate: string;
+  apartment: string | null;
+  totalCost: number;
+  createdBy: string | null;
+  category: Category;
+  status: string;
+  attentionDate: string;
+  closingDate: string;
+  assignedTo: AssignedTo;
+}
