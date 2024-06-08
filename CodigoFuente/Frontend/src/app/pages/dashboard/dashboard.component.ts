aa60d82e46d8df7665c7194cc8c819f7fcd73b03
Sebastian Borjas <sebastianborjas2000@gmail.com>
2024-06-08 02:21:11 -0300

    feat(dashboard): crate dashboard template

--- CodigoFuente/Frontend/src/app/pages/dashboard/dashboard.component.ts
+++ CodigoFuente/Frontend/src/app/pages/dashboard/dashboard.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { UserService } from '../../services/user.service';
+import { RoleTypes } from '../invitations/roleTypes';
 
 @Component({
   selector: 'app-dashboard',
@@ -8,5 +10,17 @@ import { Component } from '@angular/core';
   styleUrl: './dashboard.component.css'
 })
 export class DashboardComponent {
+  constructor(private _userService: UserService) { }
 
+  name: string = '';
+  email: string = '';
+  rol: string = '';
+
+  ngOnInit() {
+    this._userService.getUser().subscribe((user) => {
+      this.name = user.name;
+      this.email = user.email;
+      this.rol = user.role;
+    });
+  }
 }
