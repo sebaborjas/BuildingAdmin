0d5c1012ff96cf60c7a44c2d5caca07c71f3ee1b
Agustina Martinez <agustinamartinez1044@gmail.com>
2024-06-09 17:38:22 -0300

    feat(pages): create manager ticket component

---
+++ CodigoFuente/Frontend/src/app/pages/tickets/manager/tickets.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-manager-tickets',
+  standalone: true,
+  imports: [],
+  templateUrl: './tickets.component.html',
+  styleUrl: './tickets.component.css'
+})
+export class TicketsManagerComponent {
+
+}
