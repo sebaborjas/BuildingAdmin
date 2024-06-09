17d8a6def9f033e80386176b55a3500e54846894
Sebastian Borjas <sebastianborjas2000@gmail.com>
2024-06-09 19:32:26 -0300

    feat: add interceptor to check backend connection

--- CodigoFuente/Frontend/src/app/app.config.ts
+++ CodigoFuente/Frontend/src/app/app.config.ts
@@ -17,6 +17,7 @@ import { UserInterceptor } from './Interceptors/user.interceptor';
 import { AdminInterceptor } from './Interceptors/admin.interceptor';
 import { MOperatorInterceptor } from './Interceptors/maintenance-operator.interceptor';
 import { provideHotToastConfig } from '@ngneat/hot-toast';
+import { ConnectionInterceptor } from './Interceptors/connection-interceptor.interceptor';
 
 export const appConfig: ApplicationConfig = {
   providers: [
@@ -47,6 +48,11 @@ export const appConfig: ApplicationConfig = {
     {
       provide: HTTP_INTERCEPTORS,
       useClass: MOperatorInterceptor,
+      multi: true,
+    },
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: ConnectionInterceptor,
       multi: true,
     },
   ],
