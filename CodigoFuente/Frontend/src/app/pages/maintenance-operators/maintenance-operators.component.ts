a79b588ef58d2eee4329cf053c22aef1151eef26
Agustina Martinez <agustinamartinez1044@gmail.com>
2024-06-09 17:39:50 -0300

    feat(pages): create maintenance operators component

---
+++ CodigoFuente/Frontend/src/app/pages/maintenance-operators/maintenance-operators.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-maintenance-operators',
+  standalone: true,
+  imports: [],
+  templateUrl: './maintenance-operators.component.html',
+  styleUrl: './maintenance-operators.component.css'
+})
+export class MaintenanceOperatorsComponent {
+
+}
